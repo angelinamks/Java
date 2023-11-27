8f9b461f3cf6b47c4d3983319ef7ca94f9b78325
angelinamks <angelinamksmchk@gmail.com>
2023-11-28 00:36:38 +0100

    add 4_1 lab work

---
+++ Lab4_1/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+
+        String string = "Racecar";
+
+        Boolean isPalindrome = palindrome.isPalindrome(string);
+
+        if(isPalindrome){
+            System.out.println("String " + string + " is palindrome");
+        } else {
+            System.out.println("String " + string + " is not palindrome");
+        }
+    }
+}
